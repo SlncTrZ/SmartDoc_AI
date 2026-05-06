a12b7bb8914868bfc7494db2b1b0b067023193dd
Truong Cong Dinh <truongcongdinh97tcd@gmail.com>
2026-05-06 23:37:55 +0700

    Fix: Remove dual mode state - UploadZone self-manages mode

--- frontend/src/components/TabInput.js
+++ frontend/src/components/TabInput.js
@@ -1,22 +1,9 @@
 const React = require('react');
 
 class TabInput extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            mode: props.operationMode || 'hybrid',
-        };
-    }
-
-    handleModeToggle(newMode) {
-        this.setState({ mode: newMode });
-    }
-
     render() {
         const UploadZone = window.UploadZoneComponent;
         return UploadZone ? React.createElement(UploadZone, {
-            operationMode: this.state.mode,
-            onModeToggle: (m) => this.handleModeToggle(m),
             onDocumentProcessed: this.props.onDocumentProcessed,
         }) : null;
     }
